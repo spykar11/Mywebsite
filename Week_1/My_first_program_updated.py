eba925186d4224d50f663c886ef58f50caa92941
spykar11 <63569437+spykar11@users.noreply.github.com>
2020-05-07 23:39:40 +1000

    Add files via upload

---
+++ Week_1/My_first_program_updated.py
@@ -0,0 +1,100 @@
+#Sunny 
+##02 May 2020
+##This is my first program 
+
+print("Hello and Welcome")
+import time
+print("This is laughter challengen interraction")
+time.sleep(1)
+
+print("Just getting ready...")
+
+print("Loading.0%")
+time.sleep(1)
+print("Loading..65%")
+time.sleep(2)
+print("Loading...99%")
+time.sleep(2)
+
+
+print("My name is Robo")
+time.sleep(2)
+
+answer1 = input("What is your name?")
+time.sleep(2)
+print(f"Welcome {answer1}, It is a pleasure to meet you!")
+
+time.sleep(2)
+print("Lets get to know each other. I was just born last night, How old are you?")
+time.sleep(3)
+
+age = int(input("Please enter your age here:"))
+
+time.sleep(2)
+
+if age > 25:
+    print("WOW! That is even more than I can count. You are very very old my friend, Sorry!")
+else:
+    print("Jesus! You have a long bumpy way ahead of you. ENJOY!!!")
+
+time.sleep(2)
+
+print("Soooo, what do you do for living?")
+time.sleep(1)
+answer2 = input("Please type your answer here:")
+time.sleep(2)
+
+print("Hmm, I haven't meet someone who is {answer2}, interesting line of work.")
+time.sleep(2)
+
+print(f"{answer1} let me tell you what i am going to do for you today!")
+      
+time.sleep(2)
+print("I am going to make a deal with you")
+time.sleep(3)
+print("I am going to tell you 2 jokes, and if you laugh on any of my jokes you are going to buy me a Single Malt Oil Martini for my parts")
+time.sleep(3)
+
+print("Otherwise, I will buy you pizza! I hope you are ready!")
+      
+time.sleep(2)
+      
+print('A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on." The  child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now." The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family.')
+
+time.sleep(7)
+
+joke_1 = input("Did you laugh: ")
+      
+if joke_1 == yes:
+    print("Nailed it!")
+else:
+    print("I still have one more go.")
+      
+time.sleep(2)
+      
+print('Long time ago, people who sacrifice their sleep, family, food, laughter and other joys of life were called SAINTS.')
+time.sleep(1)
+print('But now, they are called IT professionals!')
+      
+joke_2 = input("Did you laugh: ")
+      
+if joke_2 == yes:
+    print("Nailed it again!")
+else:
+    print("Hmm, I wounder WHY?")
+      
+time.sleep(3)
+      
+print("As per our deal you now have to but me Single Malt Extra Oil Martini for my parts.")
+time.sleep(3)
+      
+print('Hope you ENJOYED it, see you soon again')
+#answer3 = input("Please type your answer here")
+
+
+#My_first_program.py
+
+
+
+
+#name = input ("What is your name?")
